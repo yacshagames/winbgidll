99128ed2f2d07419a0e38988fec0a8dd0dd67139
yacshagames <ramondc@hotmail.com>
2024-04-01 18:09:57 -0500

    New: wherex, wherey, textcolor, cgetch and ckbhit functions are added
    - wherex, wherey, textcolor, cgetch and ckbhit functions are added
    - Comments are added to the declaration of each function
    - If WinBGI is not used, the 16 default colors are defined for console mode
    - The cgetch and ckbhit functions are defined in console mode (conio.h), so as not to cause the same conflict with the WinBGI getch and kbhit functions.

--- conio.cpp
+++ conio.cpp
@@ -7,12 +7,16 @@
 #include <fstream>
 #include <time.h>
 
+// Developed by Alejandro Caro
+// https://stackoverflow.com/a/75297939/8486034
 void gotoxy(int x, int y)
 {
 	COORD c = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
+// Developed by Microsoft
+// https://learn.microsoft.com/en-us/windows/console/clearing-the-screen
 void cls(HANDLE hConsole)
 {
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
@@ -50,19 +54,78 @@ void cls(HANDLE hConsole)
 	SetConsoleCursorPosition(hConsole, csbi.dwCursorPosition);
 }
 
+// Developed by Yacsha
 void clrscr() {
 
-	HANDLE hStdout;
-	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 	cls(hStdout);
 }
 
+// Developed by Yacsha
 // Generates a random from 0 to randomMax
 int random(int randomMax) {
 	return rand() % (randomMax +1);
 }
 
+// Developed by Yacsha
 // Start the seed to generate random numbers. Used with the random function
 void randomize() {
 	srand((unsigned int)time(0));
+}
+
+// Developed by Alejandro Caro
+// https://stackoverflow.com/a/75297939/8486034
+int wherex(){
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	return csbi.dwCursorPosition.X;
+}
+
+// Developed by Alejandro Caro
+// https://stackoverflow.com/a/75297939/8486034
+int wherey(){
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	return csbi.dwCursorPosition.Y;
+}
+
+// Developed by vegaseat
+// https://www.daniweb.com/programming/software-development/code/216345/add-a-little-color-to-your-console-text
+void textcolor(int color) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, color); // Console font color
+}
+
+// Developed by helloacm
+// https://helloacm.com/modern-getch-implementation-on-windows-cc/
+char cgetch() {
+
+	DWORD mode, cc;
+	HANDLE h = GetStdHandle(STD_INPUT_HANDLE);
+
+	if (h == NULL) {
+		return 0; // console not found
+	}
+
+	GetConsoleMode(h, &mode);
+	SetConsoleMode(h, mode & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT));
+	TCHAR c = 0;
+	ReadConsole(h, &c, 1, &cc, NULL);
+	SetConsoleMode(h, mode);
+	return c;
+}
+
+// Developed by CodeMonkey
+// https://cboard.cprogramming.com/cplusplus-programming/13937-kbhit-portability.html
+bool ckbhit(){
+
+	HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
+	INPUT_RECORD ip;//Holds keyboard event data
+	DWORD dwResult;
+
+	ReadConsoleInput(hIn, &ip, 1, &dwResult);
+	if (ip.EventType == KEY_EVENT)
+		return true;
+
+	return false;
 }
--- conio.h
+++ conio.h
@@ -1,6 +1,28 @@
-/*******************************
-Conio.h
+#pragma once
+/****************************************************************
+CONIO.H
 Old Borland C++ and Turbo C functions. Not Standard
+
+The code of this library has been programmed by different authors,
+to whom I thank. I have only been in charge of organizing and
+grouping these functions in this library:
+
+	JOSE LUIS DE LA CRUZ LAZARO
+	ramondc@hotmail.com
+
+	YACSHA - Software & Desing
+	>> The World of chaos - EL MUNDO DEL CAOS - Unlimited Programming
+
+HISTORY...
+
+Version 3 - 01-IV-2024
+- wherex, wherey, textcolor, cgetch and ckbhit functions are added
+- Comments are added to the declaration of each function
+- If WinBGI is not used, the 16 default colors are defined for
+  console mode
+- The cgetch and ckbhit functions are defined in console mode
+  (conio.h), so as not to cause the same conflict with the WinBGI
+  getch and kbhit functions.
 
 Version 2 - 27-III-2024
 randomize function added
@@ -8,16 +30,27 @@ randomize function added
 Version 1 - 20-III-2024
 gotoxy, cls, clrscr, random functions are added
 
-By Yacsha
-*******************************/
-
-#pragma once
+****************************************************************/
 #include <Windows.h>
 
+#ifndef WINBGI_H
+
+// If WinBGI is not used, the 16 default colors are defined for console mode
+
+enum colors {
+	BLACK, BLUE, GREEN, CYAN, RED, MAGENTA, BROWN, LIGHTGRAY, DARKGRAY,
+	LIGHTBLUE, LIGHTGREEN, LIGHTCYAN, LIGHTRED, LIGHTMAGENTA, YELLOW, WHITE
+};
+
+#endif 
+
+// Move the console cursor to the x, y position
 void gotoxy(int x, int y);
 
+// Clear the console screen, pointed by hConsole
 void cls(HANDLE hConsole);
 
+// Clean the console screen
 void clrscr();
 
 // Generates a random from 0 to randomMax
@@ -25,3 +58,19 @@ int random(int randomMax);
 
 // Start the seed to generate random numbers. Used with the random function
 void randomize();
+
+// Gets the x coordinate of the current position of the console cursor
+int wherex();
+
+// Gets the y coordinate of the current position of the console cursor
+int wherey();
+
+// choose the color of the text displayed in the console
+void textcolor(int color);
+
+// Pauses the console until the user presses a key on the console
+// The code(ASCII) of the key pressed is returned, or zero otherwise
+char cgetch();
+
+// Returns true if the user has pressed a key on the console, or false otherwise
+bool ckbhit();
